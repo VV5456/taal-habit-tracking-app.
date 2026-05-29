2cb1add5d3456f8dbad0b536153e9871ac1dff80
Vedansh Vaidya <vvproxy123@gmail.com>
2026-05-29 11:37:25 +0530

    Changes to test why backend deployment is failing

--- backend/src/main/java/com/vedansh/taal/TaalApplication.java
+++ backend/src/main/java/com/vedansh/taal/TaalApplication.java
@@ -1,10 +1,20 @@
 package com.vedansh.taal;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaalApplication {
+	@Bean
+	CommandLineRunner testEnv() {
+		return args -> {
+			System.out.println("=================================");
+			System.out.println("DB_URL = " + System.getenv("DB_URL"));
+			System.out.println("=================================");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaalApplication.class, args);
